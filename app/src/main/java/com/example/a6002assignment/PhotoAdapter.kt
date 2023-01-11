506604546a7def873f4250b4087192ec74a290f3
wafrsbsg <darwelldavid@gmail.com>
2023-01-12 04:25:16 +0800

    add Photo Function_get and display data of realtime database

---
+++ app/src/main/java/com/example/a6002assignment/PhotoAdapter.kt
@@ -0,0 +1,43 @@
+package com.example.a6002assignment
+
+import Users
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlin.collections.ArrayList
+
+class PhotoAdapter(private val users: ArrayList<Users>, private val context: Context) :
+    RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val pic1 : ImageView
+        val comment1 : TextView
+        init {
+            pic1 = view.findViewById(R.id.iv_viewPhoto)
+            comment1 = view.findViewById(R.id.tv_comment)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //Create new view
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_photo, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Get the contents of the element replacement view from Users
+        //Use Glide to load photo
+        Glide.with(context).load(users[position].pic).into(holder.pic1)
+        holder.comment1.text = users[position].comment
+    }
+
+    //Return the size of Users
+    override fun getItemCount() = users.size
+}
