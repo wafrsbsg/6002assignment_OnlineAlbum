53fe37cdef91e84de7f568437d41c28d7b66129a
wafrsbsg <darwelldavid@gmail.com>
2023-01-12 22:39:40 +0800

    update Photo Function_UI update

---
+++ app/src/main/java/com/example/a6002assignment/MicGpsActivity.kt
@@ -0,0 +1,142 @@
+package com.example.a6002assignment
+
+import android.Manifest
+import android.content.Context
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.location.*
+import android.os.Bundle
+import android.speech.RecognizerIntent
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.ktx.database
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import java.util.*
+
+
+class MicGpsActivity : AppCompatActivity(), LocationListener {
+    private val REQUEST_CODE_MIC = 1
+    var storage = FirebaseStorage.getInstance()
+    var storageRef = storage.getReferenceFromUrl("gs://assignment-d3570.appspot.com")
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_mic_gps)
+        val gpsButton = findViewById<Button>(R.id.btn_gps)
+        val micButton = findViewById<Button>(R.id.btn_mic)
+        val permission = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+
+        //gps part
+        gpsButton.setOnClickListener {
+            //request permission
+            request.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+            //Check if the application has permissions when the gpsButton is clicked
+            //If the permission already exists, use the requestLocationUpdates method to get the user's current location
+            if ( permission == PackageManager.PERMISSION_GRANTED) {
+                val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+                locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0f, this)
+            }
+        }
+
+
+        //mic part
+        micButton.setOnClickListener {
+                //Call the speech recognizer when the micButton is clicked
+                val intent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH)
+                startActivityForResult(intent, REQUEST_CODE_MIC)
+        }
+
+        title = "Upload comment and filming location"
+    }
+
+    //request permission
+    private val request = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { granted ->
+        if (granted){
+            //If the user grant, the application will be able to access the precise location
+            Manifest.permission.ACCESS_FINE_LOCATION
+        }else{
+            //If the user does not grant, show toast
+            Toast.makeText(applicationContext, "Please give permission", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    //Display speech recognition results and upload the results to realtime database
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val resultView = findViewById<TextView>(R.id.tv_result)
+        //Get user ID and unique key from CameraActivity
+        val getIntent = intent
+        val uid = intent.getStringExtra("uid")
+        val key = intent.getStringExtra("key")
+
+        super.onActivityResult(requestCode, resultCode, data)
+        //checking request code with result code
+        if (requestCode == REQUEST_CODE_MIC) {
+            //Check if data is null
+            if (data != null) {
+                //Extract data from array list
+                val result = data?.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS) as ArrayList<String>
+                //Display the result in resultView
+                resultView.text = "Your Comment: "+result[0]
+
+                val uploadMicButton: Button = findViewById(R.id.btn_uploadMic)
+                uploadMicButton.setOnClickListener {
+                    //Use the setValue method to write result to the specific location of the firebase realtime database,
+                    //uid and key Get from CameraActivity
+                    var database: DatabaseReference = Firebase.database.reference
+                    database.child("user").child(uid.toString()).child(key.toString())
+                        .child("comment").setValue(result[0])
+                    Toast.makeText(applicationContext, "Upload Comment Succeeded", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    //Upload the user's current location to the realtime database
+    override fun onLocationChanged(p0: Location) {
+        val textAddress = findViewById<TextView>(R.id.tv_address)
+        val uploadGpsButton = findViewById<Button>(R.id.btn_uploadGps)
+        //Get user ID and unique key from CameraActivity
+        val uid = intent.getStringExtra("uid")
+        val key = intent.getStringExtra("key")
+
+        //Use the Geocoder method to convert latitude and longitude into addresses
+        val geocoder = Geocoder(this, Locale.getDefault())
+        val addresses: List<Address> = geocoder.getFromLocation(p0.latitude, p0.longitude, 1)
+        val address: String = addresses[0].getAddressLine(0)
+
+        //Text view displays address. When the user's location changes, address will also change
+        textAddress!!.text = "Your Address: "+address
+
+        uploadGpsButton.setOnClickListener {
+            //Use the setValue method to write latitude, longitude and accress to the specific location of the firebase realtime database,
+            //uid and key Get from CameraActivity
+            var database: DatabaseReference = Firebase.database.reference
+            database.child("user").child(uid.toString()).child(key.toString()).child("latitude")
+                .setValue(p0.latitude)
+            database.child("user").child(uid.toString()).child(key.toString()).child("longitude")
+                .setValue(p0.longitude)
+            database.child("user").child(uid.toString()).child(key.toString()).child("address")
+                .setValue(address)
+            Toast.makeText(applicationContext,"Upload Address Succeeded",Toast.LENGTH_LONG).show()
+        }
+    }
+
+    fun gotoMain(view: View) {
+        ////When the button is clicked, the user ID will be sent to MainActivity
+        val uid = intent.getStringExtra("uid")
+        val intent = Intent(this, MainActivity::class.java)
+        intent.putExtra("uid", uid);
+        startActivity(intent)
+    }
+}
+
---
+++ app/src/main/java/com/example/a6002assignment/PhotoAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.a6002assignment
+
+import Users
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlin.collections.ArrayList
+
+class PhotoAdapter(private val users: ArrayList<Users>, private val context: Context) :
+    RecyclerView.Adapter<PhotoAdapter.ViewHolder>() {
+
+    class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val viewPhoto: ImageView
+        val viewComment: TextView
+        val viewLocation: TextView
+        init {
+            viewPhoto = view.findViewById(R.id.iv_viewPhoto)
+            viewComment = view.findViewById(R.id.tv_comment)
+            viewLocation = view.findViewById(R.id.tv_location)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        //Create new view
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_photo, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        //Get the contents of the element replacement view from Users
+        //Use Glide to load photo
+        Glide.with(context).load(users[position].pic).into(holder.viewPhoto)
+        holder.viewComment.text = users[position].comment
+        holder.viewLocation.text = users[position].address
+
+        //When users click itemView, they will send users' element
+        // to PhotoDetailActivity and go to PhotoDetailActivity.
+        // At that time, users can see the details of the clicked photos
+        holder.itemView.setOnClickListener {
+            val pic = users[position].pic
+            val latitude = users[position].latitude
+            val longitude = users[position].longitude
+            val intent = Intent(context,PhotoDetailActivity::class.java)
+            intent.putExtra("pic",pic)
+            intent.putExtra("latitude",latitude)
+            intent.putExtra("longitude",longitude)
+            context.startActivity(intent)
+        }
+    }
+
+    //Return the size of Users
+    override fun getItemCount() = users.size
+}
